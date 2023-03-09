1d4e4087b1a1e9b28c122f15b6d2a251f4b8a52d
Josiah Glyshaw <josiah@glyshaw.com>
2023-03-08 18:49:34 -0600

    react init

---
+++ Frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Dashboard from './components/Dashboard';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <p>hello world</p>
+    </div>
+  );
+}
+
+export default App;
