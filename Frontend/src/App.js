d15483d0d6f6554b3cc1933055ef7ba5dcf829f3
Josiah Glyshaw <josiah@glyshaw.com>
2023-03-11 13:45:03 -0600

    added routing

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,12 +1,34 @@
 import logo from './logo.svg';
-import Dashboard from './components/Dashboard';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Page1 from './pages/page1';
+import Menu from "./components/Menu";
 
 function App() {
   return (
-    <div>
-      <p>hello world</p>
-    </div>
+    <>
+    
+    
+    
+
+    <BrowserRouter>
+    <Menu/>
+
+        <Routes>
+          <Route index element={<p>Too lazy to make home page</p>} />
+          <Route path="page1" element={<Page1/>} />
+
+        </Routes>
+      </BrowserRouter>
+    
+    
+    
+    
+    
+    
+    </>
+   
+
+
   );
 }
 
